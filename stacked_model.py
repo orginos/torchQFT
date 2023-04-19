c685b752659e6cde720e4402c69272ede2d2e29f
Kostas Orginos <kostas@wm.edu>
2023-04-19 12:15:56 -0400

    finished stacked model

--- stacked_model.py
+++ stacked_model.py
@@ -58,7 +58,11 @@ class SuperModel(nn.Module):
     def loss(self,x):
         return (self.log_prob(x)+self.target(x)).mean()
 
+    def diff(self,x):
+        return self.log_prob(x)+self.target(x)
+    
 def trainSM( SuperM, levels=[], epochs=100,batch_size=16,super_batch_size=1,learning_rate=1.0e-4):
+    tic = time.perf_counter()
     params = []
     if levels==[] :
         params = [p for p in SuperM.parameters() if p.requires_grad==True]
@@ -89,20 +93,23 @@ def trainSM( SuperM, levels=[], epochs=100,batch_size=16,super_batch_size=1,lear
         #    toc=time.perf_counter()
         #    print('iter %s:' % t, 'loss = %.3f' % loss,'time = %.3f' % (toc-tic),'seconds')
         #    tic=time.perf_counter()
+    toc = time.perf_counter()
+    print(f"Time {(toc - tic):0.4f} seconds")
     return loss_history
 
 
-def plot_loss(lh):
+def plot_loss(lh,title):
     plt.plot(np.arange(len(lh)),lh)
     plt.xlabel("epoch")
     plt.ylabel("KL-divergence")
     plt.title("Training history of MG super model ")
-    plt.show()
-    #title = "L="+str(L)+"-batch="+str(batch_size)+"-LR="+str(learning_rate)
+    #plt.show()
+    plt.savefig("sm_tr_"+title+".pdf")
+    plt.close()
 
-def validate(batch_size,mm):
+def validate(batch_size,title,mm):
     x=mm.sample(batch_size)
-    diff = (o.action(x)+mm.log_prob(x)).detach()
+    diff = mm.diff(x).detach()
     m_diff = diff.mean()
     diff -= m_diff
     print("max  action diff: ", tr.max(diff.abs()).numpy())
@@ -116,23 +123,19 @@ def validate(batch_size,mm):
 
     print("mean re-weighting factor: " , w.mean().numpy())
     print("std  re-weighting factor: " , w.std().numpy())
-    
-    #plt.show()
-    #title = "L"+str(L)+"-batch"+str(batch_size)+"-LR"+str(learning_rate)
-    #plt.savefig("sm_mg_train_"+title+".pdf")
-    #plt.show()
-    #plt.close()
+
     logbins = np.logspace(np.log10(1e-3),np.log10(1e3),int(w.shape[0]/10))
     _=plt.hist(w,bins=logbins)
     plt.xscale('log')
     plt.title('Reweighting factor')
-    #plt.savefig("sm_mg_rw_"+title+".pdf")
-    plt.show()
-    #plt.close()
+    plt.savefig("sm_rw_"+title+".pdf")
+    #plt.show()
+    plt.close()
     _=plt.hist(diff.detach(),bins=int(w.shape[0]/10))
     plt.title('ΔS distribution')
-    #plt.savefig("sm_mg_ds_"+title+".pdf")
-
+    plt.savefig("sm_ds_"+title+".pdf")
+    #plt.show()
+    plt.close()
 
 def test_reversibility():
     device = "cuda" if tr.cuda.is_available() else "cpu"
@@ -181,6 +184,57 @@ def test_reversibility():
     print("Should be zero: ",dd/(x.shape[0]*x.shape[1]*x.shape[2]))
     print(f"Time {(toc - tic):0.4f} seconds")
 
+def test_train(depth=1,epochs=100,load_flag=False,file="model.dict"):
+    import time
+    
+    device = "cuda" if tr.cuda.is_available() else "cpu"
+    print(f"Using {device} device")
+
+    L=16
+    batch_size=32
+    V=L*L
+    lam =1.0
+    mass= -0.2
+    o  = p.phi4([L,L],lam,mass,batch_size=batch_size)
+    phi = o.hotStart()
+    #set up a prior
+    normal = distributions.Normal(tr.zeros(V,device=device),tr.ones(V,device=device))
+    prior= distributions.Independent(normal, 1)
+
+    width=16
+    Nlayers=1
+    bij = lambda: m.FlowBijector(Nlayers=Nlayers,width=width)
+    mg = lambda : m.MGflow([L,L],bij,m.RGlayer("average"),prior)
+    models = []
+    print("Initializing ",depth," stages")
+    for d in range(depth):
+        models.append(mg())
+        
+    sm = SuperModel(models,target =o.action )
+
+    c=0
+    for tt in sm.parameters():
+        #print(tt.shape)
+        if tt.requires_grad==True :
+            c+=tt.numel()
+
+
+    tag = str(L)+"_m"+str(mass)+"_l"+str(lam)+"_st_"+str(depth)
+    if(load_flag):
+        sm.load_state_dict(tr.load(file))
+        sm.eval()
+    print("parameter count: ",c)
+    
+    for b in [4,8,16,32]:
+        loss_hist=trainSM(sm,levels=[], epochs=epochs,batch_size=b,super_batch_size=1)
+        tt = tag+"_b"+str(b)
+        plot_loss(loss_hist,tt)
+        validate(1024,tt,sm)
+
+    if(not load_flag):
+        file = "sm_phi4_"+str(L)+"_m"+str(mass)+"_l"+str(lam)+"_st_"+str(depth)+".dict"
+    tr.save(sm.state_dict(), file)
+   
 def main():
     import argparse
     import sys
@@ -188,12 +242,21 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', default='revers')
     parser.add_argument('-l', default='no-load')
-
+    parser.add_argument('-d', type=int,default=1)
+    parser.add_argument('-e', type=int,default=1000)
+    
     args = parser.parse_args()
     if(args.t=='revers'):
         print("Testing reversibility")
         test_reversibility()
-    
+    elif(args.t=="train"):
+        print("Testing MGflow training")
+        if(not args.l=="no-load"):
+            test_train(args.d,args.e,True,args.l)
+        else:
+            test_train(args.d,args.e)
+    else:
+        print("Nothing to test")
 
 if __name__ == "__main__":
     main()
