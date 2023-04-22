e88de68ea6104f504587a819d06faae2593f7c4a
Kostas Orginos <kostas@wm.edu>
2023-04-22 13:19:10 -0400

    added new tricks

---
+++ check_stacked.py
@@ -0,0 +1,82 @@
+import numpy as np
+import torch as tr
+import torch.nn as nn
+from torch import distributions
+from torch.nn.parameter import Parameter
+import phi4_mg as m
+import phi4 as p
+import integrators as i
+import update as u
+
+import time
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+import argparse
+import sys
+    
+import time
+from stacked_model import *
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', default='no-load')
+parser.add_argument('-d', type=int,default=1)
+parser.add_argument('-L', type=int,default=16)
+parser.add_argument('-m', type=float,default=-0.5)
+parser.add_argument('-g', type=float,default=1.0)
+parser.add_argument('-b', type=int,default=128)
+
+args = parser.parse_args()
+
+file=args.f
+if(args.f=="no-load"):
+     load_flag=False
+else:
+    load_flag=True
+    file=args.f
+
+device = "cuda" if tr.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+
+depth = args.d
+L=args.L
+batch_size=args.b
+
+V=L*L
+lam =args.g
+mass=args.m
+
+o  = p.phi4([L,L],lam,mass,batch_size=batch_size)
+phi = o.hotStart()
+
+#set up a prior
+normal = distributions.Normal(tr.zeros(V,device=device),tr.ones(V,device=device))
+prior= distributions.Independent(normal, 1)
+
+width=16
+Nlayers=1
+bij = lambda: m.FlowBijector(Nlayers=Nlayers,width=width)
+mg = lambda : m.MGflow([L,L],bij,m.RGlayer("average"),prior)
+models = []
+
+print("Initializing ",depth," stages")
+for d in range(depth):
+    models.append(mg())
+        
+sm = SuperModel(models,target =o.action )
+
+c=0
+for tt in sm.parameters():
+    #print(tt.shape)
+    if tt.requires_grad==True :
+        c+=tt.numel()
+print("parameter count: ",c)
+
+tag = str(L)+"_m"+str(mass)+"_l"+str(lam)+"_st_"+str(depth)
+sm.load_state_dict(tr.load(file))
+sm.eval()
+
+print("starting model check")
+validate(batch_size,'foo',sm)
+
---
+++ train_stacked_fixed_bj.py
@@ -0,0 +1,115 @@
+import numpy as np
+import torch as tr
+import torch.nn as nn
+from torch import distributions
+from torch.nn.parameter import Parameter
+import phi4_mg as m
+import phi4 as p
+import integrators as i
+import update as u
+
+import time
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+import argparse
+import sys
+    
+import time
+from stacked_model import *
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', default='no-load')
+parser.add_argument('-d', type=int,default=1)
+parser.add_argument('-e', type=int,default=1000)
+parser.add_argument('-L', type=int,default=16)
+parser.add_argument('-m', type=float,default=-0.5)
+parser.add_argument('-g', type=float,default=1.0)
+parser.add_argument('-b', type=int,default=4)
+parser.add_argument('-nb',type=int,default=4) # number different batch sizes to use
+parser.add_argument('-lr',type=float,default=1e-4)
+
+args = parser.parse_args()
+
+file=args.f
+if(args.f=="no-load"):
+     load_flag=False
+else:
+    load_flag=True
+    file=args.f
+
+device = "cuda" if tr.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+
+depth = args.d
+L=args.L
+batch_size=args.b
+epochs= args.e
+
+V=L*L
+lam =args.g
+mass=args.m
+
+o  = p.phi4([L,L],lam,mass,batch_size=batch_size)
+phi = o.hotStart()
+
+#set up a prior
+normal = distributions.Normal(tr.zeros(V,device=device),tr.ones(V,device=device))
+prior= distributions.Independent(normal, 1)
+
+width=16
+Nlayers=2
+fixed_Bijector1 = m.FlowBijector(Nlayers=Nlayers,width=width)
+fixed_Bijector2 = m.FlowBijector(Nlayers=Nlayers,width=width)
+
+class BijectorFactory():
+     def __init__(self, bj1,bj2):
+          self.counter=-1
+          self.bj1 = bj1
+          self.bj2 = bj2
+     def bij(self):
+          self.counter+=1 
+          if (self.counter % 2 == 0):
+               print("Adding Bijector 1")
+               return self.bj1
+          else:
+               print("Adding Bijector 2")
+               return self.bj2
+
+bij = BijectorFactory(fixed_Bijector1,fixed_Bijector2)
+     
+
+mg = lambda : m.MGflow([L,L],bij.bij,m.RGlayer("average"),prior)
+models = []
+
+print("Initializing ",depth," stages")
+for d in range(depth):
+    models.append(mg())
+        
+sm = SuperModel(models,target =o.action )
+
+c=0
+for tt in sm.parameters():
+    #print(tt.shape)
+    if tt.requires_grad==True :
+        c+=tt.numel()
+print("parameter count: ",c)
+
+tag = str(L)+"_m"+str(mass)+"_l"+str(lam)+"_st_"+str(depth)+"_fbj"
+if(load_flag):
+    sm.load_state_dict(tr.load(file))
+    sm.eval()
+
+    
+for b in batch_size*(2**np.arange(args.nb)):
+     print("Running with batch_size = ",b, " and learning rate= ",args.lr)
+     loss_hist=trainSM(sm,levels=[], epochs=epochs,batch_size=b,super_batch_size=1,learning_rate=args.lr)
+     tt = tag+"_b"+str(b)
+     plot_loss(loss_hist,tt)
+     validate(1024,tt,sm)
+
+if(not load_flag):
+    file = "sm_phi4_"+tag+".dict"
+tr.save(sm.state_dict(), file)
+   
