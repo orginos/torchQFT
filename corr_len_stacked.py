37906da21248daf1b6843b37ea91ca27d942d924
Kostas Orginos <kostasorginos@gmail.com>
2024-12-28 11:12:33 +0200

    added correlation measurement by fixing old code

--- corr_len_stacked.py
+++ corr_len_stacked.py
@@ -51,6 +51,32 @@ def reweight_list(l,w):
     return (np.array(l)*np.array(w)).tolist()
 
 
+def compute_observables(av_phi,lC2p,lchi_m,E):
+    m_phi, e_phi = average(av_phi)
+    print("m_phi: ",m_phi,e_phi)
+
+    m_chi_m, e_chi_m = average(np.array(lchi_m) - (m_phi**2)*V)
+    m_C2p, e_C2p     = average(lC2p)
+    print("Chi_m: ",m_chi_m, e_chi_m)
+    print("C2p  : ",m_C2p, e_C2p)
+    avE,eE = average(E)
+    print("E = ", avE ,'+/-',eE)
+    
+    xi = correlation_length(L,m_chi_m, m_C2p)
+    print("The correlation length is: ",xi)
+    jphi   = jackknife(av_phi)
+    jchi_m = jackknife(lchi_m)- np.array(jphi)**2 * V
+    jC2p   = np.array(jackknife(lC2p)) 
+    
+    
+    j_xi = correlation_length(L,jchi_m,jC2p)
+    
+    m_xi,e_xi = average(j_xi)
+
+
+    print("The correlation length from jackknife is is: ",m_xi," +/- ", e_xi)
+
+    
 parser = argparse.ArgumentParser()
 parser.add_argument('-f' , default='no-load')
 parser.add_argument('-d' , type=int  , default=1   )
@@ -60,7 +86,12 @@ parser.add_argument('-g' , type=float, default=1.0 )
 parser.add_argument('-b' , type=int  , default=1024)
 parser.add_argument('-w' , type=int  , default=16  )
 parser.add_argument('-nl', type=int  , default=1   )
-
+parser.add_argument('-sb' , type=int  , default=1    )
+parser.add_argument('-nc' , type=int  , default=1    )
+parser.add_argument('-fbj', type=bool , default=False)
+parser.add_argument('-sbj', type=bool , default=False)
+parser.add_argument('-dev', type=int  , default=-1)
+parser.add_argument('-nmeas', type=int  , default=10)
 
 args = parser.parse_args()
 
@@ -71,12 +102,38 @@ else:
     load_flag=True
     file=args.f
 
-device = "cuda" if tr.cuda.is_available() else "cpu"
+device = tr.device("cpu")
+# Check that MPS is available
+if not tr.backends.mps.is_available():
+    if not tr.backends.mps.is_built():
+        print("MPS not available because the current PyTorch install was not "
+              "built with MPS enabled.")
+    else:
+        print("MPS not available because the current MacOS version is not 12.3+ "
+              "and/or you do not have an MPS-enabled device on this machine.")
+
+else:
+     if (args.dev >= 0 ):
+          device = tr.device("mps")
+
+# Check that CUDA is available
+if not tr.cuda.is_available():
+    if not tr.backends.cuda.is_built():
+        print("CUDA not available because the current PyTorch install was not "
+              "built with CUDA enabled.")
+    else:
+        print("CUDA not available or you do not have a GPU on this machine.")
+
+else:
+     if (args.dev >= 0 ):
+          device = tr.device("cuda:"+str(args.dev))
+
 print(f"Using {device} device")
 
 depth = args.d
 L=args.L
 batch_size=args.b
+Nconvs = args.nc
 
 V=L*L
 lam =args.g
@@ -91,15 +148,33 @@ prior= distributions.Independent(normal, 1)
 
 width=args.w
 Nlayers=args.nl
+
+#non parity symmetric (default) bijector
 bij = lambda: m.FlowBijector(Nlayers=Nlayers,width=width)
-mg = lambda : m.MGflow([L,L],bij,m.RGlayer("average"),prior)
+if(args.fbj):
+     bij_list = []
+     for k in range(2*Nconvs):
+          bij_list.append(m.FlowBijector(Nlayers=Nlayers,width=width))
+     bij = m.BijectorFactory(bij_list).bij
+
+#use parity symmetric bijector
+if(args.sbj):
+     print("Using parity symmetric bijector")
+     bij = lambda: m.FlowBijectorParity(Nlayers=Nlayers,width=width)
+     if(args.fbj):
+          bij_list = []
+          for k in range(2*Nconvs):
+               bij_list.append(m.FlowBijectorParity(Nlayers=Nlayers,width=width))
+          bij = m.BijectorFactory(bij_list).bij
+
+mg = lambda : m.MGflow([L,L],bij,m.RGlayer("average"),prior,Nconvs=Nconvs)
 models = []
 
 print("Initializing ",depth," stages")
 for d in range(depth):
     models.append(mg())
         
-sm = SuperModel(models,target =o.action )
+sm = SuperModel(models,target =o.action)
 
 c=0
 for tt in sm.parameters():
@@ -108,14 +183,23 @@ for tt in sm.parameters():
         c+=tt.numel()
 print("parameter count: ",c)
 
-sm.load_state_dict(tr.load(file))
-sm.eval()
+tag = str(L)+"_m"+str(mass)+"_l"+str(lam)+"_w_"+str(width)+"_l_"+str(Nlayers)+"_nc_"+str(Nconvs)+"_st_"+str(depth)
+if(args.fbj):
+     tag = tag + "_fbj"
+if(args.sbj):
+     tag = tag + "_sbj"
+if(load_flag):
+    sm.load_state_dict(tr.load(file))
+    sm.eval()
+
+sm.to(device)
+
 
 print("starting model check")
-validate(batch_size,'foo',sm)
+validate(batch_size,10*args.sb,'foo',sm)
 
 
-Nmeas = 10
+Nmeas = args.nmeas
 
 lC2p = []
 lchi_m = []
@@ -142,34 +226,19 @@ for k in range(Nmeas):
     lC2p.extend(C2p.detach().numpy())
     lchi_m.extend(chi_m.numpy())
 
+print("RAW observables")
+compute_observables(av_phi,lC2p,lchi_m,E)
+print("-------")
+
 av_phi = reweight_list(av_phi,rw)
 lC2p = reweight_list(lC2p,rw)
 lchi_m = reweight_list(lchi_m,rw)
 E = reweight_list(E,rw)
 
-m_phi, e_phi = average(av_phi)
-print("m_phi: ",m_phi,e_phi)
+print("Reweighted observables")
+compute_observables(av_phi,lC2p,lchi_m,E)
+print("-------")
 
-m_chi_m, e_chi_m = average(np.array(lchi_m) - (m_phi**2)*V)
-m_C2p, e_C2p     = average(lC2p)
-print("Chi_m: ",m_chi_m, e_chi_m)
-print("C2p  : ",m_C2p, e_C2p)
-avE,eE = average(E)
-print("E = ", avE ,'+/-',eE)
-
-xi = correlation_length(L,m_chi_m, m_C2p)
-print("The correlation length is: ",xi)
-jphi   = jackknife(av_phi)
-jchi_m = jackknife(lchi_m)- np.array(jphi)**2 * V
-jC2p   = np.array(jackknife(lC2p)) 
-
-
-j_xi = correlation_length(L,jchi_m,jC2p)
-
-m_xi,e_xi = average(j_xi)
-
-
-print("The correlation length from jackknife is is: ",m_xi," +/- ", e_xi)
 
 plt.hist(rw,bins=100)
 plt.show()
