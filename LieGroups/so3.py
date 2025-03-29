9716a6cf4aabc6aa0d9e529092aae8ff1e11d330
Kostas Orginos <kostasorginos@gmail.com>
2025-03-29 09:27:15 +0100

    made smooth exponential maps

--- LieGroups/so3.py
+++ LieGroups/so3.py
@@ -25,12 +25,26 @@ def check_algebra():
 #SO(3) specific exponentiation
 def expo(X):
     #print("This is so3 expo:")
+    nX = (X.norm(dim=(-1,-2))/np.sqrt(2)).unsqueeze(-1).unsqueeze(-1)+tr.finfo(X.dtype).eps
+    return tr.eye(3).expand(*(X.shape[:-2]),-1,-1) + tr.sin(nX)/nX*X + (1-tr.cos(nX))/(nX*nX)*(X@X)
+    
+
+#this is non-smooth so it have issues with autodiff
+def expo_old(X):
+    #print("This is so3 expo:")
     nX = (X.norm(dim=(-1,-2))/np.sqrt(2)).unsqueeze(-1).unsqueeze(-1)
     R = tr.eye(3).expand(*(X.shape[:-2]),-1,-1) + tr.sin(nX)/nX*X + (1-tr.cos(nX))/(nX*nX)*(X@X)
     return  tr.where(nX<tr.finfo(X.dtype).eps,tr.eye(3).expand(*(X.shape[:-2]),-1,-1),R)
 
 #SO(3) specific differential of the exponential map
 def dexpo(X,Y):
+    nX = (X.norm(dim=(-1,-2))/np.sqrt(2)).unsqueeze(-1).unsqueeze(-1)+tr.finfo(X.dtype).eps
+    adj = ad(X,Y)
+    nX2= nX**2
+    return Y - (1-tr.cos(nX))/(nX2)*adj + (1 - tr.sin(nX)/nX)/(nX2) * ad(X,adj)
+
+# this is non-smooth so it have issues with autodiff
+def dexpo_old(X,Y):
     nX = (X.norm(dim=(-1,-2))/np.sqrt(2)).unsqueeze(-1).unsqueeze(-1)
     adj = ad(X,Y)
     nX2= nX**2
@@ -49,3 +63,10 @@ def check_expo_and_dexpo():
     dexpoXYiter = taylor_dexpo(X,Y)
     tt=(dexpoXYiter - dexpoXY).norm()/dexpoXYiter.norm()
     print("If zero dexpo works:",tt.numpy())
+
+    expoX_old = expo_old(X)
+    ((expoX-expoX_old).norm()/expoX.norm())
+    print("If check old vs new expo: ",tt.numpy())
+    dexpoXY_old = dexpo_old(X,Y)
+    tt=(dexpoXY_old - dexpoXY).norm()/dexpoXYiter.norm()
+    print("If check old vs new dexpo: ",tt.numpy())
