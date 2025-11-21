2f687e64e1340405d49be142fc42e7a04c396be5
Yamil Cahuana <yacahuanamedra@gulf.sciclone.wm.edu>
2025-11-21 15:01:23 -0500

    new version, test HMC still needed

--- README.md
+++
@@ -1,8 +0,0 @@
-# torchQFT
-
-Implements Multi-Grid flows.
-
-
-# Notes
-We may want to look to : https://github.com/rtqichen/torchdiffeq
-if we want to add the functionality of gradient flows as bijectors.
--- lattice_uni_db/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from .schema import RunMeta, Observables
-from .writer import LatticeDBWriter
-from .loader import LatticeRun, LatticeDataset, query_index
-__all__ = ["RunMeta", "Observables", "LatticeDBWriter", "LatticeRun", "LatticeDataset", "query_index"]
